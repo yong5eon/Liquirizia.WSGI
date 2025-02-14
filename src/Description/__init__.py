603ffd8b6c3dd29d14ae8cd826ae401facbbe044
허용선 <yongseon.heo@gmail.com>
2025-02-14 18:05:05 +0900

    RequestDescription 데코레이터 추가

--- src/Description/__init__.py
+++ src/Description/__init__.py
@@ -39,6 +39,21 @@ from .Auth import (
 	OpenIdConnect,
 )
 
+from .Descriptor import Descriptor
+from .Documentation import (
+	Information,
+	Contact,
+)
+
+from ..Properties import (
+	RequestRunner,
+	RequestStreamRunner,
+	RequestServerSentEventsRunner,
+	RequestWebSocketRunner,
+)
+
+from typing import Type, Union, Optional, Sequence
+
 __all__ = (
 	## Description
 	'Description',
@@ -76,4 +91,45 @@ __all__ = (
 	'KeyCookie',
 	'TLS',
 	'OpenIdConnect',
+	# Descriptor
+	'Descriptor',
+	# Documentation
+	'Information',
+	'Contact',
+	# Decorator
+	'RequestDescription',
 )
+
+
+class RequestDescription(object):
+	def __init__(
+		self,
+		summary: str,
+		description: str,
+		parameters: Optional[Union[DescriptionRequestParameter,Sequence[DescriptionRequestParameter]]] = None,
+		headers: Optional[Union[DescriptionRequestHeader,Sequence[DescriptionRequestHeader]]] = None,
+		qs: Optional[Union[DescriptionRequestQueryString,Sequence[DescriptionRequestQueryString]]] = None,
+		body: DescriptionRequestBodyProperties = None,
+		responses: Optional[Union[DescriptionResponse,Sequence[DescriptionResponse]]] = None,
+		auth: DescriptionRequestAuth = None,
+		tags: Optional[Union[str, Sequence[str]]] = None,
+	):
+		self.description = Description(
+			summary=summary,
+			description=description,
+			parameters=parameters,
+			headers=headers,
+			qs=qs,
+			body=body,
+			responses=responses,
+			auth=auth,
+			tags=tags
+		)
+		return
+	def __call__(self, obj: Type[Union[RequestRunner,RequestStreamRunner,RequestServerSentEventsRunner,RequestWebSocketRunner]]):
+		descriptor = Descriptor()
+		descriptor.add(
+			obj=obj,
+			description=self.description,
+		)
+		return obj
