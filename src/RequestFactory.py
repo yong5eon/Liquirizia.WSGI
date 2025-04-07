f3ff3eb839c6ce43cccbd7255c455cfe179755bb
허용선 <yongseon.heo@gmail.com>
2025-04-07 19:37:09 +0900

    RouteRun 을 RequestFactory 로 변경

--- src/RequestFactory.py
+++ src/RequestFactory.py
@@ -9,13 +9,12 @@ from .ResponseWriter import ResponseWriter
 from abc import ABCMeta, abstractmethod
 
 __all__ = (
-	'RouteRun'
+	'RequestFactory'
 )
 
 
-class RouteRun(metaclass=ABCMeta):
-	"""RouteRun Interface"""
-
+class RequestFactory(metaclass=ABCMeta):
+	"""RequestFactory Interface"""
 	@abstractmethod
 	def run(self, request: Request, reader: RequestReader, writer: ResponseWriter) -> Response:
 		raise NotImplementedError('{} must be implemented run'.format(self.__class__.__name__))
