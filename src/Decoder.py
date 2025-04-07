f5cbf443214459adb83b505f7c9d3f753cbcfc84
허용선 <yongseon.heo@gmail.com>
2025-04-07 19:33:44 +0900

    불필요한 인터페이스 제거

--- src/Decoder.py
+++ src/Decoder.py
@@ -14,11 +14,3 @@ class Decoder(metaclass=ABCMeta):
 	@abstractmethod
 	def __call__(self, body: bytes) -> Any:
 		raise NotImplemented('{} must be implemented __call__'.format(self.__class__.__name__))
-	@property
-	@abstractmethod
-	def format(self):
-		raise NotImplemented('{} must be implemented format'.format(self.__class__.__name__))
-	@property
-	@abstractmethod
-	def charset(self):
-		raise NotImplemented('{} must be implemented charset'.format(self.__class__.__name__))
--- src/Encoder.py
+++ src/Encoder.py
@@ -14,12 +14,3 @@ class Encoder(metaclass=ABCMeta):
 	@abstractmethod
 	def __call__(self, body: Any) -> bytes:
 		raise NotImplemented('{} must be implemented __call__'.format(self.__class__.__name__))
-	@property
-	@abstractmethod
-	def format(self):
-		raise NotImplemented('{} must be implemented format'.format(self.__class__.__name__))
-	@property
-	@abstractmethod
-	def charset(self):
-		raise NotImplemented('{} must be implemented charset'.format(self.__class__.__name__))
-
