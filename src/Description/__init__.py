b8f8e383d93c048904884247ef2c2288149eb987
허용선 <yongseon.heo@gmail.com>
2025-04-11 17:18:15 +0900

    RequestDescription 데코레이터 제거

--- src/Description/__init__.py
+++ src/Description/__init__.py
@@ -27,8 +27,6 @@ from Liquirizia.Description import Value, Schema
 from typing import Type, Union, Optional, Sequence, Dict
 
 __all__ = (
-	# Decorator
-	'RequestDescription',
 	# Descriptor
 	'Descriptor',
 	# Description
@@ -46,45 +44,3 @@ __all__ = (
 	'License',
 	'Tag',
 )
-
-
-class RequestDescription(object):
-	def __init__(
-		self,
-		summary: str,
-		description: str,
-		tags: Optional[Union[str, Sequence[str]]] = None,
-		method: str = None,
-		url: str = None,
-		parameters: Optional[Dict[str, Value]] = None,
-		headers: Optional[Dict[str, Value]] = None,
-		qs: Optional[Dict[str, Value]] = None,
-		body: Body = None,
-		responses: Optional[Union[Response,Sequence[Response]]] = None,
-		auth: Auth = None,
-	):
-		self.description = Description(
-			summary=summary,
-			description=description,
-			tags=tags,
-			method=method,
-			url=url,
-			parameters=parameters,
-			headers=headers,
-			qs=qs,
-			body=body,
-			responses=responses,
-			auth=auth,
-		)
-		return
-	def __call__(
-		self,
-		obj: Type[Union[
-			RequestRunner,
-			RequestStreamRunner,
-			RequestServerSentEventsRunner,
-			RequestWebSocketRunner
-		]]):
-		descriptor = Descriptor()
-		descriptor.add(self.description)
-		return obj
