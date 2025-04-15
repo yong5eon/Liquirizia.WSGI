b21870801ad6d9f0ba9248aa72621a13eb6503fb
허용선 <yongseon.heo@gmail.com>
2025-04-15 17:12:59 +0900

    TypeEncoder 인터페이스 추가

--- src/Encoder.py
+++ src/Encoder.py
@@ -6,6 +6,7 @@ from typing import Any
 
 __all__ = (
 	'Encoder',
+	'TypeEncoder',
 )
 
 
@@ -13,4 +14,11 @@ class Encoder(metaclass=ABCMeta):
 	"""Content Encoder Interface"""
 	@abstractmethod
 	def __call__(self, body: Any) -> bytes:
+		raise NotImplemented('{} must be implemented __call__'.format(self.__class__.__name__))
+
+
+class TypeEncoder(metaclass=ABCMeta):
+	"""Type Reader Interface"""
+	@abstractmethod
+	def __call__(self, o: Any) -> Any:
 		raise NotImplemented('{} must be implemented __call__'.format(self.__class__.__name__))
