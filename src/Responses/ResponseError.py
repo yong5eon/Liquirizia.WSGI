ea891425b74a45ad3d104e637ad6a5dbc6d9f81d
허용선 <yongseon.heo@gmail.com>
2025-05-14 19:42:57 +0900

    에러 응답 변경

--- src/Responses/ResponseError.py
+++ src/Responses/ResponseError.py
@@ -11,24 +11,12 @@ __all__ = (
 class ResponseError(Response):
 	"""Response Error Class"""
 	def __init__(self, error: Error):
-		headers = error.headers if error.headers else {}
-		body = None
-		format = None
-		charset = None
-		if error.body:
-			body = error.body
-			format = error.format
-			charset = error.charset
-		else:
-			body = error.traceback.encode('utf-8')
-			format = 'text/plain'
-			charset = 'utf-8'
 		super(ResponseError, self).__init__(
 			status=error.status,
 			message=error.message,
-			headers=headers,
-			body=body,
-			format=format,
-			charset=charset,
+			headers=error.headers,
+			body=error.body,
+			format=error.format,
+			charset=error.charset,
 		)
 		return
